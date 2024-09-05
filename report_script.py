47edb2811f8ea007af2499552086cee31a19bf9b
Alexander Karanja <alexanderkaranja78@gmail.com>
2024-09-06 01:30:12 +0300

    done ✅

--- report_script.py
+++ report_script.py
@@ -11,4 +11,16 @@ def generate_report():
         print(f"Total number of thr entries: {len(entries)}\n")
         print(f"{'Timestamp':<20} | Description")
         print('-'* 50)
-        
+
+        for entry in entries:
+            timestamp, description = entry.split('- ')
+            print(f"{timestamp:<20} | {description.strip()}")
+
+    except FileNotFoundError:
+        print("Log.txt  file not found. Make sure the file exists.")
+
+
+if __name__ == "__main__":
+    command = input("Enter 'report' to generate the report:").strip().lower()
+    if command == 'report':
+        generate_report
